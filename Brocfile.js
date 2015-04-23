80975241575b36b16da6e3ee5f8de3bdce83be49
jonpitch <jon.pitcherella@gmail.com>
2015-04-22 21:21:26 -0400

    fixing the header

--- Brocfile.js
+++ Brocfile.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp({
   sassOptions: {
     includePaths: ['bower_components/materialize/sass']
-  },
+  }
 });
 
 module.exports = app.toTree();
