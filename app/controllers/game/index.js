f052d2e09f3fcdb886fba11af68cfcceef6a1658
jonpitch <jon.pitcherella@gmail.com>
2015-04-25 17:29:45 -0400

    basic UI for a game.

---
+++ app/controllers/game/index.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  actions: {
+
+    // start or resume the game
+    resumeGame: function () {
+
+    },
+
+    // end the current game
+    endGame: function () {
+      
+    }
+  }
+});
