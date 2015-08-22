154f77ec242126ce121645754b18c33552c72108
jonpitch <jon.pitcherella@gmail.com>
2015-08-22 13:43:07 -0400

    base 3rd party libraries

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,23 +1,34 @@
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
+  const env = EmberApp.env();
+
+  const app = new EmberApp(defaults, {
+    // ...
+    sassOptions: {
+      includePaths: ['bower_components/materialize/sass']
+    },
+
+    // jscs
+    jscsOptions: {
+      enabled: true,
+      esnext: true
+    }
+
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  // bring in materialize
+  app.import('bower_components/materialize/dist/font/roboto/Roboto-Regular.tff', {
+    destDir: 'font/roboto'
+  });
+  app.import('bower_components/materialize/dist/font/roboto/Roboto-Regular.woff', {
+    destDir: 'font/roboto'
+  });
+  app.import('bower_components/materialize/dist/font/roboto/Roboto-Regular.woff2', {
+    destDir: 'font/roboto'
+  });
+  app.import('bower_components/materialize/dist/js/materialize.min.js');
 
   return app.toTree();
 };
